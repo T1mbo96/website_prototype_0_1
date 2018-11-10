4bf6d9e40ab3ae03c9c93a9a77939a04506f9679
Timo <TimoM96@outlook.de>
2018-11-10 14:47:09 +0100

    added pages

--- website_prototype_0_1/urls.py
+++ website_prototype_0_1/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import  include
 
 urlpatterns = [
+    path('Name/', include('Prototype_0_1.urls')),
     path('admin/', admin.site.urls),
 ]
